67f916974e125774c519bf7fa30265822ac8096a
H1Kt0 <konovalchuk.denis@gmail.com>
2024-05-25 18:52:38 +0300

    теперь доска может вернуть игровую координату данного тайла.

--- StarTrek/engine/gameObjects/desk.js
+++ StarTrek/engine/gameObjects/desk.js
@@ -1,5 +1,7 @@
 import Tile from "./tile.js";
 import GameObject from "./gameObject.js";
+import Figure from "./figure.js";
+import ChessPosition from "../chessPosition.js";
 
 const constants = {
     layersCount: 3,
@@ -13,17 +15,24 @@ const lowerLayerPredicate = element => element < 2;
 
 class Desk extends GameObject {
     #desks;
+    #yGap;
 
     get () {
-        if (arguments.length === 3) {
+        if (arguments.length === 1 && arguments[0] instanceof Figure) {
+            return this.#getTileByFigure(arguments[0]);
+        } else if (arguments.length === 3) {
             return this.#getByPoints(arguments[0], arguments[1], arguments[2]);
-        } else {
+        } else if (arguments.length === 1) {
             return this.#getByPosition(arguments[0]);
         }
     }
 
     #getByPosition (position) {
         return this.#getByPoints(position.layer, position.x, position.z);
+    }
+
+    #getTileByFigure(figure) {
+        return this.#desks.flat(Infinity).find(t => t.figure === figure);
     }
 
     #getByPoints(layer, x, z) {
@@ -40,10 +49,20 @@ class Desk extends GameObject {
         return this.#desks[layer-1][z-1][x-1];
     }
 
+    getTilePosition(tile) {
+        var y = (this.position.y - tile.position.y) / this.#yGap;
+        var x = (tile.position.x - this.position.x) / Tile.size.width - 2*y;
+        var z = (tile.position.z - this.position.z) / Tile.size.depth;
+
+        return new ChessPosition(3-y, x+1, z+1);
+    }
+
     constructor(x, y, z, yGap) {
         super(x, y, z);
 
+        this.#yGap = yGap;
         this.#desks = []
+        
         this.#desks.push(this.#createLayer(x + 4*Tile.size.width, y - 2*yGap, z, lowerLayerPredicate));
         this.#desks.push(this.#createLayer(x + 2*Tile.size.width, y - yGap, z, middleLayerPredicate));
         this.#desks.push(this.#createLayer(x, y, z, topLayerPredicate));
