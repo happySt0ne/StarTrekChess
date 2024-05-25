8e22e58ab8d4691fddd220cc7ccb756c29170e91
H1Kt0 <konovalchuk.denis@gmail.com>
2024-05-25 16:51:10 +0300

    Теперь есть синглтон класс game, в котором инкапсулирована основная логика игры.

--- StarTrek/main.js
+++ StarTrek/main.js
@@ -1,16 +1,10 @@
 "use strict";
-import * as shader from './shaders/shadersPackage.js'
-import cameraController from './engine/cameraController.js';
-import Tile from './engine/gameObjects/tile.js';
-import GameObject from './engine/gameObjects/gameObject.js';
-import Desk from './engine/gameObjects/desk.js';
 import { 
-    setShaderColorAttrib, 
-    setShaderPositionAttrib 
+    setShaderColorAttrib, setShaderPositionAttrib 
 } from './supportStuff/shaderAttributes.js';
-import Figure from './engine/gameObjects/figure.js';
-import FigureController from './engine/figureController.js';
-import figureTypes from './engine/figureTypes.js';
+
+import * as shader from './shaders/shadersPackage.js'
+import Game from './engine/game.js';
 
 function main() { 
     var gl = document.querySelector("#canvas").getContext("webgl");
@@ -33,34 +27,22 @@ function main() {
     var colorBuffer = gl.createBuffer();
     
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    
-    FigureController.setupUiController();
 
-    cameraController.enableCameraController();
-    
-    GameObject.setBuffers(positionBuffer, colorBuffer);
-    GameObject.setMatrixLocation(matrixLocation);
-    
-    var c = new Figure(0, 0, -500, 'black', figureTypes.Pawn);
-    var c2 = new Figure(0, 0, 0, 'white', figureTypes.Knight);
-    var c3 = new Figure(0, 0, -500, 'black', figureTypes.Pawn);
+    var setupData = {
+        positionBuffer : positionBuffer,
+        colorBuffer : colorBuffer,
+        matrixLocation: matrixLocation
+    };
 
-    var desk = new Desk(-300, 200, -500, 200);
+    var game = new Game(setupData);
 
-    FigureController.setDesk(desk);
-
-    var gameObjects = [
-        desk,
-        c, c2, c3
-    ];
-
-    gameObjects[0].get(3, 3, 1).setFigure(c);
-    gameObjects[0].get(2, 3, 3).setFigure(c2);
-    gameObjects[0].get(1, 2, 1).setFigure(c3);
+    gl.useProgram(program);
 
     setInterval(drawScene, 30);
-
+    
     function drawScene() {
+        setShaderPositionAttrib(positionLocation, positionBuffer);
+        setShaderColorAttrib(colorLocation, colorBuffer);
 
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -69,14 +51,7 @@ function main() {
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.enable(gl.DEPTH_TEST);
 
-        gl.useProgram(program);
-
-        setShaderPositionAttrib(positionLocation, positionBuffer);
-
-        setShaderColorAttrib(colorLocation, colorBuffer);
-
-        // Дальше можно отрисовывать объекты.
-        gameObjects.forEach((e) => e.draw());
+        game.draw();
     }
 }
 
