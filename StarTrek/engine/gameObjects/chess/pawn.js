b788208baa402bfb365c7143c4b73e47a42b8ab8
H1Kt0 <konovalchuk.denis@gmail.com>
2024-05-29 11:43:37 +0300

    Добавил модельки королю, слону и пешке.

--- StarTrek/engine/gameObjects/chess/pawn.js
+++ StarTrek/engine/gameObjects/chess/pawn.js
@@ -1,10 +1,13 @@
 import Figure from "../figure.js";
 import figureTypes from "../../types/figureTypes.js";
+import Size from "../../types/size.js";
 
 class Pawn extends Figure {
     constructor (x, y, z, color) {
         super(x, y, z, color, figureTypes.Pawn);
     }
+
+    static size = new Size(30, 30, 30);
 }
 
 export default Pawn;
