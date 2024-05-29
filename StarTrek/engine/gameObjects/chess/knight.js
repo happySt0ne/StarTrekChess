4b47f084cde926315fae476b7a5c22c340e119b4
H1Kt0 <konovalchuk.denis@gmail.com>
2024-05-29 13:29:36 +0300

    Добавил модельку коня.

--- StarTrek/engine/gameObjects/chess/knight.js
+++ StarTrek/engine/gameObjects/chess/knight.js
@@ -1,9 +1,41 @@
 import Figure from "../figure.js";
 import figureTypes from "../../types/figureTypes.js";
+import Size from "../../types/size.js";
+import Position from "../../types/position.js";
 
 class Knight extends Figure {
     constructor (x, y, z, color) {
         super(x, y, z, color, figureTypes.Knight);
+    }
+
+    static size = new Size(50, 5, 50);
+
+    draw() {
+        var direction = (this.color == 'black' ? 1 : -1);
+
+        super.draw();
+
+        var platformSize = new Size(20, 40, 10);
+        var platformPos = new Position(this.position);
+        platformPos.x += Math.abs(Knight.size.width - platformSize.width) / 2;
+        platformPos.z += Math.abs(Knight.size.depth - platformSize.depth) / 2;
+        platformPos.y += Knight.size.height;        
+
+        super.draw(platformPos, platformSize);
+
+        platformPos = new Position(this.position);
+        platformPos.y += platformSize.height + Knight.size.height
+        platformSize = new Size(30, 25, 20);
+        platformPos.x += Math.abs(Knight.size.width - platformSize.width) / 2;
+        platformPos.z += Math.abs(Knight.size.depth - platformSize.depth) / 2;
+
+        super.draw(platformPos, platformSize);
+
+        platformSize = new Size(45, 15, 20);
+        platformPos.x += Math.abs(Knight.size.width - platformSize.width) / 2 * direction;
+
+        super.draw(platformPos, platformSize);
+
     }
 }
 
